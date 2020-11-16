a9d731fd4c179cf2c21eaee1853f4b16bc6605f6
dheer meena <dheer1998meena@gmail.com>
2020-11-17 02:10:22 +0530

    [Dheer] Add : Welcome to Employee Payroll Rest Sharp Problem

---
+++ EmployeePayrollProblem-RestSharp/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollProblem_RestSharp
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}
